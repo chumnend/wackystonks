ca2378aa9206d54494d9975095ccc42958776e2b
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-08-18 18:50:09 -0400

    remove ws-stonk and ws-models

--- packages/ws-models/src/index.ts
+++
@@ -1,5 +0,0 @@
-import Stonk from './Stonk';
-
-export const createStonk = (name: string, symbol: string, price: number): Stonk => {
-  return new Stonk(name, symbol, price);
-}
--- packages/ws-socket/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { createStonk } from 'ws-models';
-
-const testStonk = createStonk('test', 'TST', 10.00);
-console.log(testStonk);
