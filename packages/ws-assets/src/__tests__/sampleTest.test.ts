1156f069a0141c74b0ce205977d61f799e427fc2
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-09-05 21:59:30 -0400

    setup testing framework in ws-assets

---
+++ packages/ws-assets/src/__tests__/sampleTest.test.ts
@@ -0,0 +1,10 @@
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+describe('calculate', function () {
+  it('add', function () {
+    const result = 5 + 2;
+    expect(result).equal(7);
+  });
+});
