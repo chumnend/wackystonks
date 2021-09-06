bee0760748854d2c80caaa37b0391001b67ef456
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-09-05 22:14:07 -0400

    added tests cases for Stonk class

---
+++ packages/ws-assets/tests/Stonk.test.ts
@@ -0,0 +1,23 @@
+import * as chai from 'chai';
+
+import { Stonk } from '../src';
+
+const expect = chai.expect;
+
+describe('Stonk', () => {
+  it('expects Stonk to be properly created', () => {
+    const stonk = new Stonk('Test Stonk', 'TST', 10.0);
+
+    expect(stonk.getName()).to.equal('Test Stonk');
+    expect(stonk.getSymbol()).to.equal('TST');
+    expect(stonk.getPrice()).to.equal(10);
+  });
+
+  it('expects to modify price of the Stonk', () => {
+    const stonk = new Stonk('Test Stonk', 'TST', 10.0);
+    expect(stonk.getPrice()).to.equal(10);
+
+    stonk.modifyPrice(1);
+    expect(stonk.getPrice()).to.equal(11);
+  });
+});
