13a0618289dd06ffb7329052a0cca0d6eb0ad24f
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-09-07 20:09:49 -0400

    show sent stonks on the DemoPage

--- packages/ws-client/src/pages/DemoPage/DemoPage.tsx
+++ packages/ws-client/src/pages/DemoPage/DemoPage.tsx
@@ -1,19 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { socket } from '../../socket';
 
+interface IStonk {
+  name: string;
+  symbol: string;
+  price: number;
+}
+
 const DemoPage = () => {
+  const [stonks, setStonks] = useState([]);
+
   useEffect(() => {
     socket.on('status', (msg) => {
       console.log(msg);
     });
 
     socket.on('update', (obj) => {
-      console.log(obj);
+      setStonks(obj);
     });
   }, []);
 
-  return <h2>Demo</h2>;
+  const stonksList = stonks.map((stonk: IStonk, idx) => (
+    <div key={idx}>
+      <h2>
+        {stonk.name}: {stonk.symbol} = {stonk.price}
+      </h2>
+    </div>
+  ));
+
+  return (
+    <>
+      <div>{stonksList}</div>
+    </>
+  );
 };
 
 export default DemoPage;
