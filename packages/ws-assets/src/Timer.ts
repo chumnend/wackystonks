15786ea2604347cffa1853b4398695d7078fe9c5
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-09-17 23:16:12 -0400

    updated start and pause methods for Timer

--- packages/ws-assets/src/Timer.ts
+++ packages/ws-assets/src/Timer.ts
@@ -1,23 +1,73 @@
 class Timer {
-  private id: ReturnType<typeof setInterval>;
-  private timeRemaining: number;
-  private startTime: Date;
+  private id: ReturnType<typeof setInterval> | ReturnType<typeof setTimeout>;
   private callback: () => void;
+  private delayTime: number;
+  private startTime: number;
+  private timeLeft: number;
+  private started: boolean;
+  private paused: boolean;
+  private loop: boolean;
 
-  constructor(callback: () => void, delay: number) {
+  constructor(callback: () => void, delay: number, loop = false) {
     this.callback = callback;
-    this.timeRemaining = delay;
-    this.startTime = new Date();
+    this.delayTime = delay;
+    this.timeLeft = delay;
+    this.started = false;
+    this.paused = true;
+    this.loop = loop;
   }
 
-  public resume(): void {
-    clearTimeout(this.id);
-    this.id = setTimeout(this.callback, this.timeRemaining);
+  /**
+   * Creates interval or timeout timer to execute provided callback function
+   * @param delay the interval (in milliseconds) to execute the callback
+   */
+  private _startTimer(delay: number): void {
+    if (this.loop) {
+      this.id = setInterval(this.callback, delay);
+    } else {
+      this.id = setTimeout(this.callback, delay);
+    }
   }
 
-  public pause(): void {
-    clearTimeout(this.id);
-    this.timeRemaining = new Date().getTime() - this.startTime.getTime();
+  /**
+   * Stops timer set by _startTimer method
+   */
+  private _stopTimer(): void {
+    if (this.loop) {
+      clearInterval(this.id);
+    } else {
+      clearTimeout(this.id);
+    }
+  }
+
+  /**
+   * Start or restart the timer
+   * @returns {boolean} true if started/restarted, else false
+   */
+  public start(): boolean {
+    if (this.started || !this.paused) {
+      return false;
+    }
+    this.started = true;
+    this.paused = false;
+    this.startTime = new Date().getTime();
+    this._stopTimer();
+    this._startTimer(this.delayTime);
+    return true;
+  }
+
+  /**
+   * Pauses the timer
+   * @returns {boolean} true if timer was paused, else false
+   */
+  public pause(): boolean {
+    if (!this.started || this.paused) {
+      return false;
+    }
+    this.paused = true;
+    this.timeLeft = new Date().getTime() - this.startTime;
+    this._stopTimer();
+    return true;
   }
 }
 
--- packages/ws-assets/tests/Timer.test.ts
+++ packages/ws-assets/tests/Timer.test.ts
@@ -5,18 +5,42 @@ import { Timer } from '../src';
 const expect = chai.expect;
 
 describe('Timer', function () {
-  this.timeout(3000);
+  this.timeout(1000);
 
   it('expects to call done after 100ms', (done) => {
     const timer = new Timer(() => done(), 100);
-    timer.resume();
+    expect(timer.start()).to.be.true;
+  });
+
+  it('expects to not start twice', (done) => {
+    const timer = new Timer(() => done(), 100);
+    expect(timer.start()).to.be.true;
+    expect(timer.start()).to.be.false;
   });
 
   it('expects pause to stop timer from finishing', (done) => {
-    const timer = new Timer(() => done('Test Failed'), 100); // if finishes, tests fails
-    timer.resume();
+    const timer = new Timer(() => done('test failed'), 100);
+    expect(timer.start()).to.be.true;
+    setTimeout(() => expect(timer.pause()).to.be.true, 50);
+    setTimeout(() => done(), 150);
+  });
 
-    setTimeout(() => timer.pause(), 50); // should stop timer
-    setTimeout(() => done(), 150); // if finishes, tests succeeds
+  it('expects to not pause if already paused', (done) => {
+    const timer = new Timer(() => done('test failed'), 100);
+    expect(timer.start()).to.be.true;
+    setTimeout(() => expect(timer.pause()).to.be.true, 50);
+    setTimeout(() => expect(timer.pause()).to.be.false, 80);
+    setTimeout(() => done(), 150);
+  });
+
+  it('expects to loop through 3 times', (done) => {
+    let counter = 0;
+    const timer = new Timer(() => counter++, 100, true);
+    expect(timer.start()).to.be.true;
+    setTimeout(() => {
+      timer.pause();
+      expect(counter).to.equal(3);
+      done();
+    }, 310);
   });
 });
