8d2ff13f2c3ace577465e6a91561b411ceceeb55
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-09-23 21:57:35 -0400

    added portfolio property

--- packages/ws-assets/src/Player.ts
+++ packages/ws-assets/src/Player.ts
@@ -1,17 +1,31 @@
+export interface StonkPortfolio {
+  [key: string]: number;
+}
 export interface PlayerProps {
   /** An identifier used to represent a player */
   id: string;
   /** Name of a player */
   name: string;
+  /** Details of a player's stonk portfolio */
+  portfolio: StonkPortfolio;
+}
+
+export interface PlayerMethods {
+  /** Add stonks to a palyer's portfolio */
+  addStonkToPortfolio(symbol: string, amount: number): boolean;
+  /** Reomve stonks from a player's portfolio */
+  removeStonkToPortfolio(symbol: string, amount: number): boolean;
 }
 
 class Player implements PlayerProps {
   private _id: string;
   private _name: string;
+  private _portfolio: StonkPortfolio;
 
   constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
+    this._portfolio = {};
   }
 
   get id(): string {
@@ -20,6 +34,30 @@ class Player implements PlayerProps {
 
   get name(): string {
     return this._name;
+  }
+
+  get portfolio(): StonkPortfolio {
+    return this._portfolio;
+  }
+
+  addStonkToPortfolio(symbol: string, amount: number): boolean {
+    if (symbol in this.portfolio) {
+      this.portfolio[symbol] += amount;
+    } else {
+      this.portfolio[symbol] = amount;
+    }
+
+    return true;
+  }
+
+  removeStonkToPortfolio(symbol: string, amount: number): boolean {
+    if (!(symbol in this.portfolio)) {
+      return false;
+    }
+
+    const newAmount = this.portfolio[symbol] - amount;
+    this.portfolio[symbol] = newAmount > 0 ? newAmount : 0;
+    return true;
   }
 }
 
--- packages/ws-assets/tests/Player.test.ts
+++ packages/ws-assets/tests/Player.test.ts
@@ -9,5 +9,29 @@ describe('Player', function () {
     const player = new Player('TSTR-123', 'Chester Tester');
     expect(player.id).to.equal('TSTR-123');
     expect(player.name).to.equal('Chester Tester');
+    expect(player.portfolio).to.deep.equal({});
+  });
+
+  it('expects to add/remove stonk record', function () {
+    const player = new Player('TSTR-123', 'Chester Tester');
+
+    // add new stonk
+    player.addStonkToPortfolio('TST', 10);
+    expect(player.portfolio).to.deep.equal({ TST: 10 });
+
+    // add to existing stonk
+    player.addStonkToPortfolio('TST', 5);
+    expect(player.portfolio).to.deep.equal({ TST: 15 });
+
+    // remove from existing
+    player.removeStonkToPortfolio('TST', 5);
+    expect(player.portfolio).to.deep.equal({ TST: 10 });
+
+    // remove more stonk than exists
+    player.removeStonkToPortfolio('TST', 100);
+    expect(player.portfolio).to.deep.equal({ TST: 0 });
+
+    // remove stonk that does not exist
+    expect(player.removeStonkToPortfolio('ABC', 10)).to.be.false;
   });
 });
