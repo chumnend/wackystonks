e209ac372072254615bab0f57ef058204665da8f
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-10-07 19:54:36 -0400

    added automatic creation of stonks in Game asset

--- packages/ws-assets/src/Game.ts
+++ packages/ws-assets/src/Game.ts
@@ -1,4 +1,5 @@
 import { Ticker, Timer } from './';
+import round from './utils/round';
 
 export interface GameProps {
   /** An indentifier used to represent a game */
@@ -27,10 +28,16 @@ class Game implements GameProps, GameMethods {
   private _handlers: (() => void)[];
 
   static DEFAULT_TICKER_SIMULATION_INTERVAL = 3000; // Default timer interval (ms)
+  static DEFAULT_TICKER_STONKS_AMOUNT = 5;
 
-  constructor(id: string, delay = Game.DEFAULT_TICKER_SIMULATION_INTERVAL) {
+  constructor(
+    id: string,
+    delay = Game.DEFAULT_TICKER_SIMULATION_INTERVAL,
+    numberOfStonks = Game.DEFAULT_TICKER_STONKS_AMOUNT,
+  ) {
     this._id = id;
     this._ticker = new Ticker(id);
+    this.randomizeStonks(numberOfStonks);
     this._timer = new Timer(this.tick.bind(this), delay, true);
     this._handlers = [];
   }
@@ -67,6 +74,22 @@ class Game implements GameProps, GameMethods {
     });
   }
 
+  /**
+   * Populates the ticker with random stonks
+   * @param numberOfStonks {Number} Number of stonks to add to ticker. Defaults to 5.
+   */
+  private randomizeStonks(numberOfStonks = Game.DEFAULT_TICKER_STONKS_AMOUNT): void {
+    for (let i = 0; i < numberOfStonks; i++) {
+      const name = `Test ${i + 1}`;
+      const symbol = `TST${i + 1}`;
+      const initialPrice = round((Math.random() * 10000) / 100, 2);
+      this._ticker.createStonk(name, symbol, initialPrice);
+    }
+  }
+
+  /**
+   * Triggers ticker simulation and calls subscribed callbacks every timer interval
+   */
   private tick(): void {
     this._ticker.simulate();
     this._handlers.forEach((item) => {
