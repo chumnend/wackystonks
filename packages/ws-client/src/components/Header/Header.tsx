123f3808b71f08c14c222c0aa11f649affaac2af
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-11-07 21:39:23 -0500

    removed DemoPage

--- packages/ws-client/src/components/Header/Header.tsx
+++ packages/ws-client/src/components/Header/Header.tsx
@@ -13,7 +13,6 @@ const Header = () => {
       {!auth?.isAuth && (
         <Styled.AuthNav>
           <Link to={Routes.HOME_ROUTE}>Home</Link>
-          <Link to={Routes.DEMO_ROUTE}>Demo</Link>
         </Styled.AuthNav>
       )}
     </Styled.Header>
--- packages/ws-client/src/constants/routes.ts
+++ packages/ws-client/src/constants/routes.ts
@@ -4,4 +4,3 @@ export const WITH_GAME_ROUTE = (id: string) => `/${id}`;
 export const REGISTER_ROUTE = '/register';
 export const LOGIN_ROUTE = '/login';
 export const LOGOUT_ROUTE = '/logout';
-export const DEMO_ROUTE = '/demo';
