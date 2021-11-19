2b5a7f22b4596b2f563405c1d7cb2c43f94df5af
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-11-18 20:09:58 -0500

    remove button style from session

--- packages/ws-client/src/pages/GamePage/Session/styles.ts
+++ packages/ws-client/src/pages/GamePage/Session/styles.ts
@@ -31,35 +31,6 @@ export const Stonks = styled.div`
   height: auto;
 `;
 
-export const Button = styled.button`
-  display: inline-block;
-  box-sizing: border-box;
-  padding: 0 1.6rem;
-  min-width: 50%;
-  height: 3rem;
-  line-height: 2.75rem;
-  border-radius: 5px;
-  font-weight: 400;
-  font-size: 1rem;
-  outline: none;
-  text-transform: capitalize;
-  text-align: center;
-  background-color: #fff;
-  color: #000;
-  cursor: pointer;
-  box-shadow: 0 5px 10px rgb(0, 0, 0, 0.12);
-  transition: transform 200ms;
-
-  &:hover:not([disabled]) {
-    transform: scale(1.1);
-  }
-
-  &:disabled {
-    color: grey;
-    cursor: not-allowed;
-  }
-`;
-
 export const Players = styled.div`
   height: auto;
 `;
