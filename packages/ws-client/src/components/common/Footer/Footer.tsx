d2c9f6e361e08a007147388c00955dfeedad4bf0
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-11-19 19:55:44 -0500

    fixed styling on home page

--- packages/ws-client/src/components/common/Footer/Footer.tsx
+++ packages/ws-client/src/components/common/Footer/Footer.tsx
@@ -1,20 +1,54 @@
-import * as Styled from './styles';
+import styled from 'styled-components';
+
+const Container = styled.footer`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 1em;
+  position: absolute;
+  bottom: 0;
+`;
+
+const Section = styled.div`
+  flex: 1 1 auto;
+  margin-left: auto;
+  position: relative;
+`;
+
+const Main = styled.div`
+  text-align: center;
+  font-size: 0.8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.67rem 0;
+`;
+
+const Link = styled.a`
+  color: #0070f3;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const Footer = () => {
   return (
-    <Styled.Footer>
-      <Styled.Side />
-      <Styled.Tag>
+    <Container>
+      <Section />
+      <Main>
         <span>
           WackyStonks by{' '}
-          <a href="#" target="_blank" rel="noopener noreferrer">
+          <Link href="#" target="_blank" rel="noopener noreferrer">
             Nicholas Chumney
-          </a>
+          </Link>
         </span>
-        <a href="#">View on GitHub</a>
-      </Styled.Tag>
-      <Styled.Side />
-    </Styled.Footer>
+        <Link href="#">View on GitHub</Link>
+      </Main>
+      <Section />
+    </Container>
   );
 };
 
--- packages/ws-client/src/components/common/PageWrapper/PageWrapper.tsx
+++ packages/ws-client/src/components/common/PageWrapper/PageWrapper.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const OuterDiv = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   position: relative;
   display: flex;
   flex-direction: column;
