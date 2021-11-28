9d46c9585ac26f6db07446a871b7947f503c8628
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-11-28 16:26:14 -0500

    added checkTime function to timers

--- packages/ws-assets/src/Timer.ts
+++ packages/ws-assets/src/Timer.ts
@@ -5,6 +5,8 @@ interface TimerMethods {
   stop(): boolean;
   /** Resets the timer to the inital delay time */
   reset(): boolean;
+  /** Return the remaining time (ms) */
+  checkTime(): number;
 }
 
 enum TimerMode {
@@ -13,13 +15,13 @@ enum TimerMode {
 }
 
 class Timer implements TimerMethods {
-  private id: ReturnType<typeof setInterval> | ReturnType<typeof setTimeout>;
   private callback: () => void;
   private delay: number;
+  private mode: TimerMode;
+  private timerId: ReturnType<typeof setInterval> | ReturnType<typeof setTimeout>;
   private startTime: number;
   private timeLeft: number;
   private paused: boolean;
-  private mode: TimerMode;
 
   static COUNTDOWN_TIMER = TimerMode.COUNTDOWN;
   static LOOPED_TIMER = TimerMode.LOOPED;
@@ -33,62 +35,78 @@ class Timer implements TimerMethods {
   constructor(callback: () => void, delay: number, mode: TimerMode) {
     this.callback = callback;
     this.delay = delay;
+    this.mode = mode;
+    this.startTime = null;
     this.timeLeft = delay;
     this.paused = true;
-    this.mode = mode;
   }
 
   /**
-   * Start or resumes the timer
-   * @returns {boolean} true if started/restarted, else false
+   * Start the timer
+   * @returns {boolean} true if started/resumed, else false
    */
   public start(): boolean {
+    // check if already started, if so return false
     if (!this.paused) {
       return false;
     }
-    this.paused = false;
-    this.startTime = new Date().getTime();
-    this._stopTimer();
-    this._startTimer(this.timeLeft);
+    this._startTimer();
     return true;
   }
 
   /**
-   * Stops the timer
+   * Stop the timer
    * @returns {boolean} true if timer was stopped, else false
    */
   public stop(): boolean {
+    // check if already stopped, if so return false
     if (this.paused) {
       return false;
     }
-    this.paused = true;
-    this.timeLeft = new Date().getTime() - this.startTime;
     this._stopTimer();
     return true;
   }
 
   /**
-   * Resets the timer to the inital delay time
+   * Stops the timer and resets the timer to the initial delay time
    * @returns {boolean} true if restared, else false
    */
   public reset(): boolean {
-    this.paused = true;
-    this._stopTimer();
     this.timeLeft = this.delay;
+    this._stopTimer();
     return true;
+  }
+
+  /**
+   * Returns the remaining time
+   * @returns {number} the remaining time (ms)
+   */
+  public checkTime(): number {
+    if (this.paused) {
+      return this.timeLeft;
+    }
+    return this.timeLeft - (new Date().getTime() - this.startTime);
   }
 
   /**
    * Creates interval or timeout timer to execute provided callback function
-   * @param delay the interval (in milliseconds) to execute the callback
    */
-  private _startTimer(delay: number): void {
+  private _startTimer(): void {
+    this.paused = false;
+    this.startTime = new Date().getTime();
     switch (this.mode) {
       case TimerMode.COUNTDOWN:
-        this.id = setTimeout(this.callback, delay);
+        this.timerId = setTimeout(() => {
+          this.callback();
+          // TODO: What happens to timer after it gets here?
+        }, this.timeLeft);
         break;
       case TimerMode.LOOPED:
-        this.id = setInterval(this.callback, delay);
+        this.timerId = setTimeout(() => {
+          this.callback();
+          this.timeLeft = this.delay;
+          this._startTimer();
+        }, this.timeLeft);
         break;
     }
   }
@@ -97,14 +115,10 @@ class Timer implements TimerMethods {
    * Stops timer set by _startTimer method
    */
   private _stopTimer(): void {
-    switch (this.mode) {
-      case TimerMode.COUNTDOWN:
-        clearTimeout(this.id);
-        break;
-      case TimerMode.LOOPED:
-        clearInterval(this.id);
-        break;
-    }
+    this.timeLeft = this.checkTime();
+    this.paused = true;
+    this.startTime = null;
+    clearTimeout(this.timerId);
   }
 }
 
--- packages/ws-assets/tests/Timer.test.ts
+++ packages/ws-assets/tests/Timer.test.ts
@@ -8,14 +8,16 @@ describe('Timer - Countdown', function () {
   it('expects to call done after 100ms', function (done) {
     this.timeout(200);
 
-    const timer = new Timer(() => done(), 100, Timer.COUNTDOWN_TIMER);
+    const timerFn = () => done();
+    const timer = new Timer(timerFn, 100, Timer.COUNTDOWN_TIMER);
     expect(timer.start()).to.be.true;
   });
 
   it('expects to not start twice', function (done) {
     this.timeout(200);
 
-    const timer = new Timer(() => done(), 100, Timer.COUNTDOWN_TIMER);
+    const timerFn = () => done();
+    const timer = new Timer(timerFn, 100, Timer.COUNTDOWN_TIMER);
     expect(timer.start()).to.be.true;
     expect(timer.start()).to.be.false;
   });
@@ -23,7 +25,8 @@ describe('Timer - Countdown', function () {
   it('expects stop to timer from finishing', function (done) {
     this.timeout(200);
 
-    const timer = new Timer(() => done('test failed'), 100, Timer.COUNTDOWN_TIMER);
+    const timerFn = () => done('stop did not stop the timer');
+    const timer = new Timer(timerFn, 100, Timer.COUNTDOWN_TIMER);
     expect(timer.start()).to.be.true;
     setTimeout(() => expect(timer.stop()).to.be.true, 50);
     setTimeout(() => done(), 150);
@@ -32,39 +35,84 @@ describe('Timer - Countdown', function () {
   it('expects to not stop if already stopped', function (done) {
     this.timeout(200);
 
-    const timer = new Timer(() => done('test failed'), 100, Timer.COUNTDOWN_TIMER);
+    const timerFn = () => done('stop did not stop the timer');
+    const timer = new Timer(timerFn, 100, Timer.COUNTDOWN_TIMER);
     expect(timer.start()).to.be.true;
     setTimeout(() => expect(timer.stop()).to.be.true, 50);
     setTimeout(() => expect(timer.stop()).to.be.false, 80);
     setTimeout(() => done(), 150);
   });
 
-  it('expects reset to restart the timer from initial delay', function (done) {
+  it('expects reset to stop the timer', function (done) {
     this.timeout(200);
 
-    const timer = new Timer(() => done('test failed'), 100, Timer.COUNTDOWN_TIMER);
+    const timerFn = () => done('reset did not stop the timer');
+    const timer = new Timer(timerFn, 100, Timer.COUNTDOWN_TIMER);
     expect(timer.start()).to.be.true;
+    setTimeout(() => expect(timer.reset()).to.be.true, 50);
+    setTimeout(() => done(), 150);
+  });
+
+  it('expects reset to restart timer from initial delay', function (done) {
+    this.timeout(200);
+
+    let count = 0;
+    const timerFn = () => count++;
+    const timer = new Timer(timerFn, 100, Timer.COUNTDOWN_TIMER);
+    expect(timer.start()).to.be.true;
+
+    // restart and resume the timer after 30 ms
     setTimeout(() => {
       expect(timer.reset()).to.be.true;
       expect(timer.start()).to.be.true;
-    }, 80);
+    }, 30);
+
+    // after 100ms count should still be 0
     setTimeout(() => {
-      timer.stop();
+      expect(count).to.equal(0);
+    }, 100);
+
+    // after 150ms, count should be 1 as timer completed
+    setTimeout(() => {
+      expect(count).to.equal(1);
       done();
     }, 150);
   });
+
+  it('expects checkTime to return time left on timer', function (done) {
+    this.timeout(200);
+
+    const timerFn = () => done();
+    const timer = new Timer(timerFn, 100, Timer.COUNTDOWN_TIMER);
+    expect(timer.checkTime()).to.equal(100);
+    expect(timer.start()).to.be.true;
+
+    setTimeout(() => {
+      // Time left should be 75 +/- 1ms
+      expect(timer.checkTime()).to.be.at.least(74).and.to.be.below(77);
+    }, 25);
+
+    setTimeout(() => {
+      expect(timer.stop()).to.be.true;
+      // Time left should be 50 +/- 1ms
+      expect(timer.checkTime()).to.be.at.least(49).and.to.be.below(52);
+      expect(timer.start()).to.be.true;
+    }, 50);
+  });
 });
 
+// the only difference with a LOOPED vs COUNTDOWN timer is that the LOOPED should run until stopped
 describe('Timer - Looped', function () {
   it('expects to loop through 3 times', function (done) {
     this.timeout(200);
 
-    let counter = 0;
-    const timer = new Timer(() => counter++, 50, Timer.LOOPED_TIMER);
+    let count = 0;
+    const timerFn = () => count++;
+    const timer = new Timer(timerFn, 50, Timer.LOOPED_TIMER);
     expect(timer.start()).to.be.true;
     setTimeout(() => {
-      timer.stop();
-      expect(counter).to.equal(3);
+      expect(timer.stop()).to.be.true;
+      expect(count).to.equal(3);
       done();
     }, 160);
   });
